939e6b6b75b81eb4fdd81c0ff7528e2cb964a68b
Melanie Joyce <136640719+mjbezerra@users.noreply.github.com>
2023-06-14 17:32:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+// Smooth Scrolling
+$("#navbar a").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 0
+            },
+            800
+        );
+    }
+});
+
+
+ 
+
+// Sticky menu background
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 150) {
+
+        document.querySelector("#navbar").style.opacity = 0.9;
+
+    } else {
+
+        document.querySelector("#navbar").style.opacity = 1;
+
+    }
+
+});
